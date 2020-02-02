b3e757b3a23579962ccfd51a0866d7c5236974fd
H4M5TER <zhuxx2003@163.com>
2020-02-03 06:26:12 +0800

    Customized

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 function downloadAsCanvas() {
-    domtoimage.toBlob(document.querySelector(".main")).then(blob => {
-            saveAs(blob, "cover" + new Date().getTime() + ".png");
-    });
+	domtoimage.toBlob(document.querySelector(".main")).then(blob => {
+		saveAs(blob, "cover" + new Date().getTime() + ".png");
+	});
 }
-$(()=> {
-    [".text-title", ".text-name"].forEach(q => {
-            $(q).click(() => {
-                $(q).text(prompt("Content for " + q) || $(q).first().text());
-            })
-        }
-    );
-});
+
+window.onload = () => {
+	var sheet = document.getElementById("sheet");
+	var sample = document.createElement("img");
+	sample.setAttribute("class", "sample");
+	sample.setAttribute("src", "img/fish.png");
+	for (i = 0; i < 14; i++) {
+		sample.setAttribute("order", i);
+		sample = sheet.appendChild(sample).cloneNode();
+	}
+	document.querySelectorAll(".sample").forEach(img => img.onclick = () => {
+		var input = document.getElementById("image_uploads");
+		input.click();
+		if (input.files.length === 1)
+			img.setAttribute("src", window.URL.createObjectURL(input.files[0]));
+	})
+};
