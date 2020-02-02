361b1ec5a059e30b5c4530e82a31276081ac624a
H4M5TER <zhuxx2003@163.com>
2020-02-03 06:28:34 +0800

    bug fix
    更改了input的实现方式 修复了重复上传同一文件时不触发更新图片的bug

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ window.onload = () => {
 		sample = sheet.appendChild(sample).cloneNode();
 	}
 	document.querySelectorAll(".sample").forEach(img => img.onclick = () => {
-		var input = document.getElementById("image_uploads");
+		var input = document.createElement("input");
+		input.setAttribute("type", "file");
+		input.setAttribute("accept", "image/*");
+		input.setAttribute("style", "visibility: hidden");
 		input.addEventListener("change", () => {
 			if (input.files.length === 1)
 				img.setAttribute("src", window.URL.createObjectURL(input.files[0]));
 		})
+		document.body.appendChild(input);
 		input.click();
 	})
 };
