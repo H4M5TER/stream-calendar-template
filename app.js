c6770a8218250ec508470ec25753c6200f2505ab
H4M5TER <zhuxx2003@163.com>
2020-02-03 07:03:26 +0800

    Change download file name

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+function getFormatDate() {
+	var date = new Date();
+	var strMonth = date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1;
+	var strDay = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+	var strHours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+	var strMinutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+	var strSeconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+	var currentdate = date.getFullYear() + "-" + strMonth + "-" + strDay + "_" + strHours + "-" + strMinutes + "-" + strSeconds;
+	return currentdate;
+}
+
 function downloadAsCanvas() {
 	domtoimage.toBlob(document.querySelector(".main")).then(blob => {
-		saveAs(blob, "cover" + new Date().getTime() + ".png");
+		saveAs(blob, "calendar_" + getFormatDate() + ".png");
 	});
 }
 
